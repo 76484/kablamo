c09e80f200777616ee5adf5841ac9e2b42116b23
Steven Smith <steven.76484@gmail.com>
2024-09-15 19:30:00 -0400

    add given Stopwatch and Lap components

---
+++ src/Lap.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Component, ClassAttributes } from "react";
+
+const Lap = (props: { index: number; lap: number; onDelete: () => {} }) => (
+  <div key={props.index} className="stopwatch-lap">
+    <strong>{props.index}</strong>/ {formattedSeconds(props.lap)}{" "}
+    <button onClick={props.onDelete}> X </button>
+  </div>
+);
+
+export default Lap;
